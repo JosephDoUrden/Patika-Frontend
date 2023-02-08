8fcff107a658f6943a3978cddf79d4d801ce5b5a
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-02-08 14:16:44 +0300

    update

--- JavascriptFunc/script.js
+++ JavascriptFunc/script.js
@@ -1,4 +1,4 @@
-let counter = 0
+let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
 let counterDOM = document.querySelector('#counter')
 let = increaseDOM = document.querySelector('#increase')
 let = decreaseDOM = document.querySelector('#decrease')
@@ -8,9 +8,11 @@ counterDOM.innerHTML = counter
 increaseDOM.addEventListener("click", function(){
     counter += 1
     counterDOM.innerHTML = counter
+    localStorage.setItem('counter', counter)
 })
 
 decreaseDOM.addEventListener("click", function(){
     counter -= 1
     counterDOM.innerHTML = counter
+    localStorage.setItem('counter', counter)
 })
